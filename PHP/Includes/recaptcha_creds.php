a57c37d91daed438e85125c8101f41fd7635edd5
Andre Mashraghi <andrem122@users.noreply.github.com>
2017-09-30 08:23:00 -0400

    Update recaptcha_creds.php

--- PHP/Includes/recaptcha_creds.php
+++ PHP/Includes/recaptcha_creds.php
@@ -1,6 +1,6 @@
 <?php
 //recaptcha credentials
 $recap_url = 'https://www.google.com/recaptcha/api/siteverify';
-$site   = '6LdsbjAUAAAAANHLotME2nlD7Hvzmg-_xopOU5nO';
-$secret = '6LdsbjAUAAAAANKffj5N4CKIkcuOdBBAfKTqBZhj';
+$site   = '****************************************';
+$secret = '****************************************';
 $remote_ip  = $_SERVER['REMOTE_ADDR'];
